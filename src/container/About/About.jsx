903787b4156e5e54314aff4445e79c6edd712ba1
moses-kelvin <moseskelvin683@gmail.com>
2023-05-02 12:20:19 +0100

    Fix grammatical error in About.jsx file

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -10,19 +10,19 @@ const About = () => {
       {
          title: "Myself",
          description:
-         "i have always loved the field of natural sciences since i was little. Mathematics and physics are my favourites, i love and enjoy solving problems related to these fields. Duiring my secondary school years, i discovered programming and it's math-like structure and logic. Through the help of online recoures such as programming bootcamps, documentations, e.t.c and  my developer friends, i have been able cultivate my programming skill. I must affirm that programming is one of the best thing that that ever occured to me.",
+         "i have always loved the field of natural sciences since i was little. Mathematics and physics are my favourites, i love and enjoy solving problems related to these fields. Duiring my secondary school years, i discovered programming and it's math-like structure and logic. Through the help of online recoures such as programming bootcamps, documentations, e.t.c and  my developer friends, i have been able to cultivate my programming skill. I must affirm that programming is one of the best thing that that ever occured to me.",
          imageUrl: images.avatar,
       },
       {
          title: "Frontend Developer",
          description:
-            "Love developing web applications/websites with smooth and great UI. i collaborate with my twin brother and other co-developers friends to provide solutions to problems, which comes easy for me, because i love thinking through a problem and coming up with an efficient solution. i'm concerned with the constant development and improvment of  my skill.",
+            "Love developing web applications/websites with smooth and great UI. i collaborate with my twin brother and other co-developer friends to provide solutions to problems, which comes easy for me, because i love thinking through a problem and coming up with an efficient solution. i'm concerned with the constant development and improvment of  my skill.",
          imageUrl: images.about02,
       },
       {
          title: "Education",
          description:
-             "i'm a student of the department of Geomatics (Bsc Geomatics), at the university of Benin (uniben), as of 2021 till now. I have a current cgpa of 4.54 on a 5.0 scale.",
+             "i'm a student of the department of Geomatics (Bsc Geomatics), at the university of Benin (uniben), as of 2021 till present. I have a current cgpa of 4.54 on a 5.0 scale.",
          imageUrl: images.uniben,
       },
       {
